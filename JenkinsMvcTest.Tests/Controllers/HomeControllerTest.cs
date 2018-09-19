f3f64d3082e59358b381a3c8ac79a33f6dfd7383
antsa0110 <antsa0110@gmail.com>
2018-09-19 11:40:47 +0700

    fix 8

--- JenkinsMvcTest.Tests/Controllers/HomeControllerTest.cs
+++ JenkinsMvcTest.Tests/Controllers/HomeControllerTest.cs
@@ -54,7 +54,7 @@ namespace JenkinsMvcTest.Tests.Controllers
         [TestMethod]
         public void Error()
         {
-            Assert.AreEqual(3, 12);
+            Assert.AreEqual(3, 3);
         }
     }
 }
